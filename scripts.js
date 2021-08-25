3f0dc53f74c1557381b2828129ff548244a8afee
larskebab <38043651+larskebab@users.noreply.github.com>
2021-08-25 14:31:48 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.top = "-1";
   } else {
     document.getElementById("navbar").style.top = "-100px";
   }
